4fd80bd38b6da5f211d889c2ff0d3dfbee47c7c3
AegeanYan <1203616626@qq.com>
2021-11-01 23:53:05 +0800

    11.1啊对对对

--- src/Frontend/SemanicChecker.java
+++ src/Frontend/SemanicChecker.java
@@ -92,7 +92,7 @@ public class SemanicChecker implements ASTVisitor {
             if (node.init != null){
                 node.init.accept(this);
                 if (!node.init.expr_ret.retType.equals("null") && !node.init.expr_ret.isEqual(null_type) && !node.init.expr_ret.isEqual(node.type) && !node.init.expr_ret.retType.equals(node.type.retType)) throw new SemanticError("Type not same in vardecl"+node.id , node.pos);
-                if (node.type instanceof ArrayTypeNode){
+                if (node.type instanceof ArrayTypeNode && !node.init.expr_ret.retType.equals("null") && !node.init.expr_ret.isEqual(null_type)){
                     if (!(node.init.expr_ret instanceof ArrayTypeNode) || ((ArrayTypeNode) node.type).dims != ((ArrayTypeNode) node.init.expr_ret).dims)throw new SemanticError("Type dismatch in vardecl"+node.id , node.pos);
                 }
             }
@@ -294,7 +294,7 @@ public class SemanicChecker implements ASTVisitor {
             case ASSIGNEQ -> {
                 if (!node.lhs.isAssignable)throw new SemanticError("not assignable in binar" , node.pos);
                 if (!ltype.retType.equals(node.rhs.expr_ret.retType) && !ltype.isEqual(node.rhs.expr_ret) && !node.rhs.expr_ret.isEqual(null_type))throw new SemanticError("dismatched assign" , node.pos);
-                if (ltype instanceof ArrayTypeNode){
+                if (ltype instanceof ArrayTypeNode && !node.rhs.expr_ret.isEqual(null_type) && !node.rhs.expr_ret.retType.equals("null")){
                     if (!(node.rhs.expr_ret instanceof ArrayTypeNode) || !(((ArrayTypeNode) ltype).dims == ((ArrayTypeNode) node.rhs.expr_ret).dims))throw new SemanticError("dismatched assign" , node.pos);
                 }
                 if (node.rhs.expr_ret.retType.equals("null") && node.rhs.expr_ret.isEqual(null_type)){
