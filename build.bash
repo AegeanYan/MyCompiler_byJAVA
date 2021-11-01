2a4024dfe3337260c32e8c39ab37be049b6fe98c
AegeanYan <1203616626@qq.com>
2021-11-01 23:01:22 +0800

    11.1啊对对对

--- build.bash
+++ build.bash
@@ -3,4 +3,4 @@
 set ff=UNIX
 set -e
 mkdir -p bin
-find ./src -name *.java | javac -d bin -cp /ulib/java/antlr-4.7.2-complete.jar @/dev/stdin
+find ./src -name *.java | javac -d bin -cp /ulib/java/antlr-4.9.2-complete.jar @/dev/stdin
