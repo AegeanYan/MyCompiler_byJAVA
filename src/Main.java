b2dafa5cec5567d759ba2bca30a5687de8b9060e
AegeanYan <1203616626@qq.com>
2022-01-31 09:50:19 +0800

    添加了内建函数

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ import Backend.*;
 public class Main{
     public static void main(String[] args) throws Exception {
         InputStream input = System.in;
-        boolean debug = false;
+        boolean debug = true;
         String name = "test.yx";
         if (debug)input = new FileInputStream(name);
         String asmOutputfile = "output.s";
